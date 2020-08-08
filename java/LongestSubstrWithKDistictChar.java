75e6952ac472ab9cd536bb43cd7cb07e4eddb5bf
coderquill <coderquill@gmail.com>
2020-08-08 11:08:13 +0530

    nary tree level order traversal

---
+++ java/LongestSubstrWithKDistictChar.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+class LongestSubstrWithKDistinctChar{
+
+
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		int n = sca.nextInt();
+		
+	}
+
+}
