d51b9a98b7c7a3a5509ec6ed2e3a400b08ef5d79
coderquill <coderquill@gmail.com>
2020-08-10 22:03:54 +0530

    top k frequent elements

---
+++ java/TopKFreqElements.java
@@ -0,0 +1,43 @@
+/*
+https://leetcode.com/problems/top-k-frequent-elements/
+
+Given a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+Note:
+
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+You can return the answer in any order.
+
+*/
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if(nums == null || nums.length == 0) return new int[0];
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n: nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            pq.offer(entry);
+            if(pq.size() > k) pq.poll();
+        }
+        int[] res = new int[k];
+        int index = 0;
+        while(pq.size() > 0) {
+            res[index++] = pq.poll().getKey();
+        }
+        
+        return res;
+    }
+}
